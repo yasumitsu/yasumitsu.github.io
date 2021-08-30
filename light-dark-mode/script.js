2c600d47292cc92446886f1abdb720922acf3a9c
Kenji <kenjiyasumitsu@gmail.com>
2021-08-31 03:30:45 +0900

    project done

---
+++ light-dark-mode/script.js
@@ -0,0 +1,44 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+function imageMode(color) {
+	image1.src = `img/undraw_proud_coder_${color}.svg`;
+	image2.src = `img/undraw_feeling_proud_${color}.svg`;
+	image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+function toggleTheme(isDark) {
+	nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+	textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+	toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+	isDark
+		? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+		: toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+	isDark ? imageMode('dark') : imageMode('light');
+}
+
+function switchTheme() {
+	event.target.checked
+		? (document.documentElement.setAttribute('data-theme', 'dark'),
+			localStorage.setItem('theme', 'dark'),
+			toggleTheme(true))
+		: (document.documentElement.setAttribute('data-theme', 'light'),
+			localStorage.setItem('theme', 'light'),
+			toggleTheme(false));
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+	document.documentElement.setAttribute('data-theme', currentTheme);
+	if (currentTheme === 'dark') {
+		toggleSwitch.checked = true;
+		toggleTheme(true);
+	}
+}
