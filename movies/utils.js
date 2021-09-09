65bfe7f18e2c77abda5d40a75842a12b36953403
Kenji <kenjiyasumitsu@gmail.com>
2021-09-10 06:03:22 +0900

    added new project movies fight

---
+++ movies/utils.js
@@ -0,0 +1,15 @@
+const debounce = (func, delay = 1000) => {
+	let timeoutId;
+	return (...args) => {
+		if (timeoutId) clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => {
+			func.apply(null, args);
+		}, delay);
+	};
+};
+
+function createErrorMsg() {
+	const h1 = document.createElement('h1');
+	h1.textContent = 'Movie not found!';
+	document.querySelector('#autocomplete').appendChild(h1);
+}
