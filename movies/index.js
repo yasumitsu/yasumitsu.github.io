b3e5d2c7a39c8dcdf199a97dcdab89f7f2c1b767
Kenji <kenjiyasumitsu@gmail.com>
2021-09-12 03:23:06 +0900

    autocomplete working

--- movies/index.js
+++ movies/index.js
@@ -1,14 +1,12 @@
 const fetchData = async (searchTerm) => {
-	const response = await axios.get('http://www.omdbapi.com/?', {
+	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
 			apikey: '1052433e',
 			s: searchTerm
 		}
 	});
 
-	if (response.data.Error) {
-		return createErrorMsg();
-	}
+	if (response.data.Error && input.value) return createErrorMsg();
 
 	return response.data.Search;
 };
@@ -31,6 +29,11 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async (e) => {
 	const movies = await fetchData(e.target.value);
 
+	if (!movies) {
+		dropdown.classList.remove('is-active');
+		return;
+	}
+
 	resultsWrapper.innerHTML = '';
 	dropdown.classList.add('is-active');
 	for (let movie of movies) {
@@ -42,8 +45,66 @@ const onInput = async (e) => {
         <img src="${imgSrc}"/>
         ${movie.Title}
         `;
+		option.addEventListener('click', () => {
+			dropdown.classList.remove('is-active');
+			input.value = movie.Title;
+			onMovieSelect(movie);
+		});
 
 		resultsWrapper.appendChild(option);
 	}
 };
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', (e) => {
+	if (!root.contains(e.target)) dropdown.classList.remove('is-active');
+});
+
+const onMovieSelect = async (movie) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: '1052433e',
+			i: movie.imdbID
+		}
+	});
+
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+	return `<article class="media">
+		<figure class="media-left">
+			<p class="image">
+				<img src="${movieDetail.Poster}" />
+			</p>
+		</figure>
+		<div class="media-content">
+			<div class="content">
+				<h2>${movieDetail.Title}</h1>
+				<h4>${movieDetail.Genre}</h4>
+				<p>${movieDetail.Plot}</p>
+			</div>
+		</div>
+	</article>
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.Awards}</p>
+		<p class="subtitle">Awards</p>
+	</article>
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.BoxOffice}</p>
+		<p class="subtitle">Box Office</p>
+	</article>
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.Metascore}</p>
+		<p class="subtitle">Metascore</p>
+	</article>
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.imdbRating}</p>
+		<p class="subtitle">IMDB Rating</p>
+	</article>
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.imdbVotes}</p>
+		<p class="subtitle">IMDB Votes</p>
+	</article>
+	`;
+};
--- movies/utils.js
+++ movies/utils.js
@@ -9,7 +9,11 @@ const debounce = (func, delay = 1000) => {
 };
 
 function createErrorMsg() {
+	dropdown.classList.remove('is-active');
 	const h1 = document.createElement('h1');
 	h1.textContent = 'Movie not found!';
 	document.querySelector('#autocomplete').appendChild(h1);
+	setTimeout(() => {
+		h1.textContent = '';
+	}, 2000);
 }
