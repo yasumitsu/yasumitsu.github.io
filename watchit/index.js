75e8832798849548f8da379a36786a050e8fa540
Kenji <kenjiyasumitsu@gmail.com>
2021-09-15 06:03:58 +0900

    deployed movies fight to vercel

--- watchit/index.js
+++ watchit/index.js
@@ -5,6 +5,7 @@ const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
 const { spawn } = require('child_process');
+const chalk = require('chalk');
 
 program.version('0.0.1').argument('[filename]', 'Name of a file to execute').action(async ({ filename }) => {
 	const name = filename || 'index.js';
@@ -15,8 +16,12 @@ program.version('0.0.1').argument('[filename]', 'Name of a file to execute').act
 		throw new Error(`Could not find the file '${name}'`);
 	}
 
+	let proc;
+
 	const start = debounce(() => {
-		spawn('node', [ name ], { stdio: 'inherit' });
+		if (proc) proc.kill();
+		console.log(chalk.green('>>>>> Starting process <<<<<'));
+		proc = spawn('node', [ name ], { stdio: 'inherit' });
 	}, 100);
 
 	chokidar.watch('.').on('add', start).on('change', start).on('unlink', start);
